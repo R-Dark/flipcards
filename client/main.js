ceb7a167b8eb7cb4e1dd3f251a09a2052e5a1c95
rob dark <robdark@robs-MacBook-Pro.local>
2017-09-16 18:55:41 -0500

    added ability to render users on client side

--- client/main.js
+++ client/main.js
@@ -1,12 +1,26 @@
-function loadFlipUsers(){
+let usersBtn = document.querySelector('#usersBtn')
+
+
+
+
+function loadFlipUsers() {
   fetch("http://localhost:3000/users")
     .then(function(response) {
       return response.json()
-    }).then(function(json) {
-      console.log(json)
+    })
+    .then(function(json) {
+      let users = json
+      let usersList = document.querySelector('#usersList')
+      for (var i = 0; i < users.length; i++) {
+        let li = document.createElement("li")
+        li.innerHTML = users[i].username
+        usersList.appendChild(li)
+      }
     }).catch(function(error) {
       console.log('parsing failed', error)
     })
 }
 
-loadFlipUsers();
+usersBtn.addEventListener('click', function(){
+  loadFlipUsers()
+})
