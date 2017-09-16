c8e6344ab07dd1c1c0b33e078b66d092b56be412
rob dark <robdark@robs-MacBook-Pro.local>
2017-09-16 18:07:38 -0500

    created json api, created client side and fetched said api

--- server/app.js
+++ server/app.js
@@ -2,8 +2,16 @@ const express = require("express")
 const app = express()
 const bodyParser = require("body-parser")
 const models = require('./models')
+const session = require("express-session")
 app.use( express.static('public'))
 app.use(bodyParser.json())
+var sess = {
+  secret: "robsite",
+  cookie: {},
+  saveUninitialized: true,
+  resave: true
+}
+app.use(session(sess))
 
 const usersRoutes = require("./routes/users")
 app.use(usersRoutes)
